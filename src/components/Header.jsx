034480e8ba333a8656c7f339cb48fc0836df0571
Jen Warman <jen.warman85@gmail.com>
2025-05-09 11:47:11 +0100

    Header component and initial file setup.

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import '../css/styles.css';
+
+function Header() {
+    return (
+        <header className="header">
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <a href="#" className="nav-list-link">About</a>
+            </li>
+            <li className="nav-list-item">
+              <a href="#" className="nav-list-link">Portfolio</a>
+            </li>
+            <li className="nav-list-item">
+              <a href="#contact" className="nav-list-link">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+
+export default Header;

84ff985c62088688ac6ab88260cbbfc2bc47c47e
Jen Warman <jen.warman85@gmail.com>
2025-05-09 13:44:18 +0100

    Portfolio component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import About from "./About";
 import Header from "./Header";
 
 import '../css/styles.css';
+import Portfolio from "./Portfolio";
 
 function Home() {
     return (
        <div className="container">
         <Header/>
         <About/>
+        <Portfolio/>
         </div>
     )
 }
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+function Portfolio() {
+    return (
+        <section className="portfolio" id="portfolio">
+        <div className="portfolio-text">
+          <h1 className="portfolio-heading">portfolio</h1>
+          <a href="#" className="portfolio-link">see more...</a>
+        </div>
+      </section>
+    )
+}
+
+export default Portfolio;

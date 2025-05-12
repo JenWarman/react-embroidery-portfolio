a00048fa2db11d03883f15a896f0799b60b30024
Jen Warman <jen.warman85@gmail.com>
2025-05-12 14:07:45 +0100

    Portfolio page.

--- src/components/portfolio/PortfolioGallery.jsx
+++ src/components/portfolio/PortfolioGallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import PortfolioItem from "./PortfolioItem";
 
 const images = [
@@ -13,22 +13,34 @@ const images = [
   "/Skulls.jpg",
 ];
 
-const PortfolioGallery = () => {
+function PortfolioGallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const columns = [[], [], [], []];
   images.forEach((img, idx) => {
     columns[idx % 4].push(img);
   });
 
+
   return (
-    <div className="portfolio-gallery">
-      {columns.map((colImages, colIdx) => (
-        <div className={`scroll-column col-${colIdx}`} key={colIdx}>
-          {[...colImages, ...colImages].map((img, idx) => (
-            <PortfolioItem key={idx} image={img} />
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="portfolio-gallery">
+        {columns.map((colImages, colIdx) => (
+          <div className={`scroll-column col-${colIdx}`} key={colIdx}>
+            {[...colImages, ...colImages].map((img, idx) => (
+              <PortfolioItem key={idx} image={img} onImageClick={setSelectedImage} />
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {selectedImage && (
+  <div className="image-modal" onClick={() => setSelectedImage(null)}>
+    <img src={selectedImage} alt="Full size" />
+  </div>
+)}
+
+    </>
   );
 };
 
--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-
-const PortfolioItem = ({ image }) => {
+function PortfolioItem({ image, onImageClick }) {
   return (
-    <div className="portfolio-item">
+    <div className="portfolio-item" onClick={() => onImageClick(image)}>
       <div className="image-wrapper">
+        <div className="image-overlay">
+          <p className="image-overlay-text">See More</p>
+        </div>
         <img src={image} alt="Embroidery art" />
       </div>
     </div>

8bbd53aa9bae966f968b17392d1eb83b92711e19
Jen Warman <jen.warman85@gmail.com>
2025-05-18 15:26:19 +0100

    Header media queries.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 function Header() {
-  return <header className="header">
-    <img src="Header.png" alt="" />
-  </header>;
+  return (
+    <header className="header">
+      <h1 className="header_heading-1">Jen Warman</h1>
+      <h1 className="header_heading-2">Embroidery</h1>
+    </header>
+  );
 }
 
 export default Header;
+
